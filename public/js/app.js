fca4b8b23c7db493872ef2b3479e84e702a8a188
Umesh Chandra Rapaka <60530897+ChintuRapaka@users.noreply.github.com>
2021-02-13 13:49:47 +0530

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.onload = function() {
 	var select = document.getElementById('seats');
 	for (var i = 0; i<40; i++){
@@ -7,14 +6,4 @@ window.onload = function() {
 	    option.innerHTML = i+1;
 	    select.options.add(option);
 	}
-=======
-window.onload = function() {
-	var select = document.getElementById('seats');
-	for (var i = 0; i<40; i++){
-	    var option = document.createElement('option');
-	    option.value = i+1;
-	    option.innerHTML = i+1;
-	    select.options.add(option);
-	}
->>>>>>> d3683dd45e4129d847de2aa83d36a0780d100458
 }
